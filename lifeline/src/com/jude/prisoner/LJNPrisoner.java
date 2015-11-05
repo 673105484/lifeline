ab7b736ff75d5d3a22db87a5418840417ab25014
673105484 <673105484@qq.com>
2015-11-05 18:55:19 +0800

    ...
    修改

---
+++ lifeline/src/com/jude/prisoner/LJNPrisoner.java
@@ -0,0 +1,35 @@
+package com.jude.prisoner;
+import com.jude.Prisoner;
+import com.jude.Manager;
+
+/**
+ * Created by asus on 2015/11/5.
+ */
+
+public class LJNPrisoner implements Prisoner {
+    int totalCount;
+    int totalPerson;
+
+    @Override
+    public String getName() {
+        return "Áú¿¡ÄÐ2015214051";
+    }
+
+    @Override
+    public void begin(Manager manager,int totalPerson, int totalCount) {
+        this.totalCount = totalCount;
+        this.totalPerson = totalPerson;
+    }
+
+    @Override
+    public int take(int index, int last) {
+
+        return( ((totalCount-last-1)/(totalPerson-index))/2-1)/2;
+    }
+
+    @Override
+    public void result(boolean survived) {
+
+    }
+
+}
